9d0ce302e281274ac8b8a1edecb52eb09c3b9f19
rdeinum <remco.deinum@gmail.com>
2022-06-07 14:49:11 +0200

    0

---
+++ ch08/GlobRegex.hs
@@ -0,0 +1,23 @@
+module GlobRegex(globToRegex) where
+
+import Text.Regex.Posix
+
+globToRegex :: String -> String
+globToRegex xs = '^' : globToRegex' xs ++ "$"
+
+globToRegex' :: String -> String
+globToRegex' []           = ""
+globToRegex' ('*':xs)     = ".*" ++ globToRegex' xs
+globToRegex' ('?':xs)     = '.' : globToRegex' xs
+globToRegex' ('[':'!':xs) = "[^" ++ charClass xs
+globToRegex' ('[':xs)     = '[' : charClass xs
+globToRegex' (x:xs)       = escape x ++ globToRegex' xs
+
+escape :: Char -> String
+escape x = if x `elem` escapables then "\\" ++ [x] else [x]
+  where escapables = "\\+()^$.{}|"
+
+charClass :: String -> String
+charClass (']':xs) = ']' : globToRegex' xs
+charClass (x:xs)   = x : charClass xs
+charClass []       = error "Unterminated character class"
