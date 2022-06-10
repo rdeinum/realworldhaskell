5df274bb7d6edb64b57dee3c7de60fcc830a85b5
rdeinum <remco.deinum@gmail.com>
2022-06-10 13:14:33 +0200

    0

--- ch08/Glob.hs
+++ ch08/Glob.hs
@@ -1,10 +1,10 @@
-module Glob(namesMatching) where
+module Glob(namesMatching, listDirsR, listDirsR') where
 
 import System.Directory (doesDirectoryExist, doesFileExist, getCurrentDirectory, getDirectoryContents)
-import System.FilePath (dropTrailingPathSeparator, splitFileName, (</>), pathSeparator)
+import System.FilePath (dropTrailingPathSeparator, splitFileName, (</>), pathSeparator, makeRelative)
 import System.Posix.Files(fileExist)
 import Control.Exception (handle, SomeException (SomeException))
-import Control.Monad (forM)
+import Control.Monad (forM, filterM)
 import GlobRegex (matchesGlob)
 import Llvm (LlvmCallConvention(CC_Ghc))
 
@@ -22,10 +22,14 @@ namesMatching globPattern
                 dirs <- if isPattern dirName
                         then namesMatching (dropTrailingPathSeparator dirName)
                         else return [dirName]
-                let listDir = if isPattern baseName then listMatches else listPlain
+                let listDir = case baseName of
+                        "**"                  -> listDirsR
+                        name | isPattern name -> listMatches
+                             | otherwise      -> listPlain
                 pathNames <- forM dirs $ \dir -> do
                     baseNames <- listDir dir baseName
-                    return (map (dir </>) baseNames)
+                    let baseNames' = map (dir </>) baseNames
+                    return (map (makeRelative ".") baseNames')
                 return (concat pathNames)
 
 isPattern :: String -> Bool
@@ -43,14 +47,10 @@ isUnix = pathSeparator == '/'
 
 listMatches :: FilePath -> String -> IO [String]
 listMatches dirName baseName = do
-    dirName' <- if null dirName
-                then getCurrentDirectory
-                else return dirName
+    dirName' <- if null dirName then getCurrentDirectory else return dirName
     handle errorHandler $ do
         names <- getDirectoryContents dirName'
-        let names' = if isHidden baseName
-                     then filter isHidden names
-                     else filter (not . isHidden) names
+        let names' = if isHidden baseName then filter isHidden names else filter (not . isHidden) names
         return (filter (\name -> matchesGlob name baseName isUnix) names')
     where 
         errorHandler :: SomeException -> IO [String]
@@ -66,3 +66,20 @@ listPlain dirName baseName = do
               then doesDirectoryExist dirName
               else doesNameExist (dirName </> baseName)
     return [baseName | exists]
+
+listDirsR :: FilePath -> String -> IO [String]
+listDirsR dirName baseName
+        | null dirName = listDirsR "." baseName
+        | otherwise    = do
+            dirExists <- doesDirectoryExist dirName
+            if dirExists then listDirsR' dirName else return []
+
+listDirsR' :: FilePath -> IO [FilePath]
+listDirsR' dir = do
+        dirs <- getDirectoryContents dir
+        let dirs' = filter isNotSpecialDir dirs
+        let dirs'' = map (dir </>) dirs'
+        dirs''' <- filterM doesDirectoryExist dirs''
+        dirs'''' <- forM dirs''' listDirsR'
+        return (dir : concat dirs'''')
+    where isNotSpecialDir dir = dir `notElem` [".", ".."]
