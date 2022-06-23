e7d4c98ba2516d506d73aad887141da2ce2c0c7a
rdeinum <remco.deinum@gmail.com>
2022-06-23 12:45:25 +0200

    0

---
+++ ch09/ex04/ControlledVisit.hs
@@ -0,0 +1,55 @@
+module ControlledVisit where
+
+import System.Directory (Permissions (searchable), getDirectoryContents, getPermissions, getFileSize, getModificationTime)
+import System.FilePath ( (</>) )
+import Data.Time (UTCTime)
+import Control.Monad (liftM, forM)
+import Prelude hiding (traverse)
+import Control.Exception (handle, SomeException (SomeException))
+
+data Info = Info {
+    infoPath :: FilePath,
+    infoPerms :: Maybe Permissions,
+    infoSize :: Maybe Integer,
+    infoModTime :: Maybe UTCTime
+} deriving (Eq, Ord, Show)
+
+type InfoP a = Info -> a
+
+liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
+liftP2 f ip1 ip2 i3 = ip1 i3 `f` ip2 i3
+
+andI :: InfoP Bool -> InfoP Bool -> Info -> Bool
+andI = liftP2 (&&)
+
+orI :: InfoP Bool -> InfoP Bool -> Info -> Bool
+orI = liftP2 (||)
+
+getInfo :: FilePath -> IO Info
+getInfo path = do
+    perms <- maybeIO $ getPermissions path
+    size <- maybeIO $ getFileSize path
+    modTime <- maybeIO $ getModificationTime path
+    return (Info path perms size modTime)
+
+maybeIO :: IO a -> IO (Maybe a)
+maybeIO action = handle catch $ Just <$> action
+    where catch :: SomeException -> IO (Maybe a)
+          catch _ = return Nothing
+
+traverse :: ([Info] -> [Info]) -> FilePath -> IO [Info]
+traverse order dir = do
+    entries <- getUsefulDirContents dir
+    infos <- mapM getInfo (dir : map (dir </>) entries)
+    fmap concat $ forM (order infos) $ \info -> 
+        if isDirectory info && infoPath info /= dir
+            then traverse order (infoPath info)
+            else return [info]
+
+getUsefulDirContents :: FilePath -> IO [String]
+getUsefulDirContents dir = do
+    entries <- getDirectoryContents dir
+    return (filter (`notElem` [".", ".."]) entries)
+
+isDirectory :: Info -> Bool
+isDirectory = maybe False searchable . infoPerms
---
+++ ch09/ex04/README.md
@@ -0,0 +1,17 @@
+## Exercise 4
+Take the predicates and combinators from “Gluing Predicates Together” on page 224 and make them work with our new Info type.
+
+### Answer
+
+```haskell
+type InfoP a = Info -> a
+
+liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
+liftP2 f ip1 ip2 i3 = ip1 i3 `f` ip2 i3
+
+andI :: InfoP Bool -> InfoP Bool -> Info -> Bool
+andI = liftP2 (&&)
+
+orI :: InfoP Bool -> InfoP Bool -> Info -> Bool
+orI = liftP2 (||)
+```
